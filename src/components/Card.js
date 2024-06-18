35f4cf016f3e530036a49c4315fc8fad83549057
AnshumaliSuri <anshumalis.ug21.cs@nitp.ac.in>
2024-06-18 20:18:24 +0530

    Implementing My Cart functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatchCart, useCart } from './ContextReducer';
 
 export default function Card(props) {
+    const dispatch = useDispatchCart();
+    let data = useCart();
+    const priceRef = useRef();
+    let options = props.options;
+    let price = Object.keys(options);
+    const [qty, setQty] = useState(1);
+    const [size, setSize] = useState(price[0]);
+    let foodItem = props.item;
 
-    let options=props.options;
-    let price=Object.keys(options);
+    useEffect(() => {
+        if (priceRef.current) {
+            setSize(priceRef.current.value);
+        }
+    }, [priceRef]);
+
+    const handleAddToCart = async () => {
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size, img: props.ImgSrc });
+        console.log(data);
+    };
+
+    useEffect(() => {
+        console.log("Options:", options);
+        console.log("Selected size:", size);
+        console.log("Quantity:", qty);
+        console.log("Final price:", finalPrice);
+    }, [size, qty]);
+
+    let finalPrice = qty * (options[size] ? parseInt(options[size]) : 0);
 
     return (
         <div>
             <div>
                 <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "760px" }}>
-                    <img src={props.imgsrc} className="card-img-top" alt="..." />
+                    <img src={props.foodItem.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
                     <div className="card-body">
-                        <h5 className="card-title">{props.foodName}</h5>
-                        {/* <p className="card-text">Khana.</p> */}
+                        <h5 className="card-title">{props.foodItem.name}</h5>
                         <div className="container w-100">
-                            <select className="m-2 bg-success h-100 rounded">
+                            <select className="m-2 bg-success h-100 rounded" value={size} onChange={(e) => setSize(e.target.value)}>
+                                {price.map((data) => {
+                                    return (
+                                        <option key={data} value={data}> {data} </option>
+                                    )
+                                })}
+                            </select>
+                            <select className="m-2 bg-success h-100 rounded" value={qty} onChange={(e) => setQty(parseInt(e.target.value))}>
                                 {Array.from(Array(6), (e, i) => {
                                     return (
                                         <option key={i + 1} value={i + 1}> {i + 1} </option>
                                     )
                                 })}
                             </select>
-                            <select className="m-2 bg-success h-100 rounded">
-                                {price.map((data)=>{
-                                    return <option key={data} value={data}>{data}</option>
-                                })}
-                            </select>
                             <div className='d-inline fs-6 h-100'>
-                                Total Price.
+                                ₹{finalPrice}/-
                             </div>
                         </div>
+                        <hr />
+                        <button className='btn btn-success justify-center ms-2' onClick={handleAddToCart}>Add to cart</button>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -78,7 +78,7 @@ export default function Home() {
                 .map(filterItems=>{
                   return (
                     <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
-                      <Card foodName={filterItems.name} options={filterItems.options[0]} imgsrc={filterItems.img}></Card>
+                      <Card foodItem={filterItems} options={filterItems.options[0]}></Card>
                     </div>
                   )
                 })
